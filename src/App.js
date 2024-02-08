fe736b662f4b10453240bf39cd5b3236e27459e9
frankienguyen13 <157472417+frankienguyen13@users.noreply.github.com>
2024-02-08 22:47:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ const truncate = (input, len) =>
 
 export const StyledButton = styled.button`
   padding: 10px;
-  border-radius: 50px;
+  border-radius: 48px;
   border: none;
   background-color: var(--secondary);
-  padding: 10px;
+  padding: 12px;
   font-weight: bold;
+  font-style: italic;
   color: var(--secondary-text);
   width: 100px;
   cursor: pointer;
